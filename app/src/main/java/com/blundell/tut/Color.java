c314dd89d8e067bdab2c98c854dd45f6292ea812
Blundell <paul@novoda.com>
2017-06-01 08:09:33 +0100

    simplify by pulling up class

---
+++ app/src/main/java/com/blundell/tut/Color.java
@@ -0,0 +1,27 @@
+package com.blundell.tut;
+
+final class Color {
+    private static final Color RED = new Color(50, 0, 0);
+    private static final Color YELLOW = new Color(50, 50, 0);
+    private static final Color PINK = new Color(50, 10, 12);
+    private static final Color GREEN = new Color(0, 50, 0);
+    private static final Color ORANGE = new Color(50, 22, 0);
+    private static final Color PURPLE = new Color(50, 0, 50);
+    private static final Color BLUE = new Color(0, 0, 50);
+
+    static final Color[] RAINBOW = {
+            Color.RED, Color.YELLOW, Color.PINK,
+            Color.GREEN,
+            Color.ORANGE, Color.PURPLE, Color.BLUE
+    };
+
+    int r;
+    int g;
+    int b;
+
+    private Color(int r, int g, int b) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+}
